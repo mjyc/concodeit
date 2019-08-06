1945ea1668e564c087cf50176ea8c879a9d86e70
Drick <strato95@cs.washington.edu>
2019-08-05 17:01:31 -0700

    added neck.js in public folder

--- condition2-callback/src/index.js
+++ condition2-callback/src/index.js
@@ -83,65 +83,6 @@ function stopDetectFace(id) {
   eventHandles[id].stream.removeListener(eventHandles[id].listener);
 }
 
-function runNeckExerciseApp() {
-  var result, posX, posY, faceDir, tt, tt2, id;
-
-/**
- * Describe this function...
- */
-function handle_display_message_done(result) {
-  sleep(1, _ => {
-  });
-}
-
-/**
- * Describe this function...
- */
-function handle_face_event(posX, posY, faceDir) {
-  if (tt == 0) {
-    if (faceDir == 'left') {
-      tt = 1;
-      tt2 = 0;
-      sendActionGoalCallback("RobotSpeechbubbleAction", String('GJ'), (result) => {
-        handle_display_message_done(tt);
-      });
-    }
-    if (faceDir == 'right') {
-      sendActionGoalCallback("RobotSpeechbubbleAction", String('move left'), (result) => {
-      });
-    }
-  }
-  else if (tt2 == 0) {
-    if (posX > '0.3') {
-      tt2 = 1;
-      sendActionGoalCallback("RobotSpeechbubbleAction", String('GJ2'), (result) => {
-        handle_display_message_done(tt);
-      });
-    }
-    if (posX < '0.2') {
-      sendActionGoalCallback("RobotSpeechbubbleAction", String('move right'), (result) => {
-      });
-    }
-  }
-  else {
-    sendActionGoalCallback("RobotSpeechbubbleAction", String('done with exercise'), (result) => {
-    });
-  }
-}
-
-
-// beg start_program
-cancelActionGoals();
-// end start_program
-tt = 0;
-tt2 = -1;
-sendActionGoalCallback("RobotSpeechbubbleAction", String('Turn your neck left, then move it right'), (result) => {
-});
-id = (detectFace(63338757, (posX, posY, faceDir) => {
-  handle_face_event(posX, posY, faceDir);
-  }));
-}
-
 //------------------------------------------------------------------------------
 // Block Function Definitions
 
@@ -508,5 +449,4 @@ document.getElementById("run").onclick = () => {
 // Scratch
 (async () => {
   console.log("started");
-  runNeckExerciseApp();
 })();
