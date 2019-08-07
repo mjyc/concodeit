562e4e507390c063e2ed3053bc9eaef7e4f5b423
Mino Nakura <minorunakura@gmail.com>
2019-08-07 15:09:24 -0700

    removed console.log

--- condition3-asyncmethods/src/index.js
+++ condition3-asyncmethods/src/index.js
@@ -86,12 +86,6 @@ function getNumDetectedFaces() {
     const listener = {
       next: val => {
         poses$.removeListener(listener);
-        const nosePoint = val[0].keypoints.find(kpt => kpt.part === "nose");
-        console.log(val[0].keypoints.find(kpt => kpt.part === "nose"));
-        if (nosePoint == NULL) {
-          console.log("null");
-        }
-
         callback(null, val.length);
       }
     };
@@ -137,11 +131,74 @@ async function conNeckSimple() {
     sendActionGoal("HumanSpeechbubbleAction", ['yes']);
     await sleep(1);
     cont = (await getActionResult("HumanSpeechbubbleAction"));
-    console.log(cont);
-    console.log(faceDirection);
   }
   cancelActionGoal("HumanSpeechbubbleAction");
   sendActionGoal("RobotSpeechbubbleAction", String('done'));
+}
+
+//------------------------------------------------------------------------------
+// Full Concurrent Neck Exercise
+async function fullNeckExercise() {
+  var result;
+  var action_list, i, face_direct, index;
+  cancelActionGoals();
+  sendActionGoal("RobotSpeechbubbleAction", String('Are you ready?'));
+  result = null;
+  while (result != 'yes') {
+    sendActionGoal("HumanSpeechbubbleAction", ['yes', 'no']);
+    await sleep(1);
+    result = (await getActionResult("HumanSpeechbubbleAction"));
+  }
+  index = 0;
+  sendActionGoal("RobotSpeechbubbleAction", String('Let\'s get started!'));
+  await sleep(2);
+  action_list = ['Get ready for a neck exercise! Look forward and keep your neck straight!', 
+                 'Stretch your shoulder to the left as far as possible!', 'hold a bit longer!', 
+                 'Bring your face back center', 'Stretch your shoulder to the right as far as possible!', 
+                 'keep going!'];
+  face_direct = ['Center', 'Left', 'Left', 'Center', 'Right', 'Right'];
+  var cont = "yes";
+  do {  
+    for (var i_index in action_list) {
+      i = action_list[i_index];
+      sendActionGoal("RobotSpeechbubbleAction", String(i));
+      await sleep(2);
+      var direction = face_direct[index];
+      var faceDirection;
+      faceDirection = null;
+      while (faceDirection !== direction) {
+        faceDirection = (await getHumanFaceDirection());
+        await sleep(1);
+        if (direction === "Right" || direction === "Left") {
+          if (faceDirection === "Center") {
+            sendActionGoal("RobotSpeechbubbleAction", 'Stretch the neck more to the ' + direction + ' ! You got this!');
+            await sleep(2);
+          }
+        }
+      }
+    index++;
+    }
+    sendActionGoal("RobotSpeechbubbleAction", 'You did it!');
+    await sleep(2);
+    cont = null;
+    var options = ['yes', 'no'];
+    sendActionGoal("RobotSpeechbubbleAction", 'Do you want to continue?');
+    cont = await YesOrNo();
+  } while (cont === "yes");
+}
+
+async function YesOrNo() {
+  var result;
+  result = null;
+  var options = ['yes', 'no'];
+  sendActionGoal("HumanSpeechbubbleAction", options);
+  while (options.indexOf(result) === -1) {
+    sendActionGoal("HumanSpeechbubbleAction", options);
+    await sleep(1);
+    result = (await getActionResult("HumanSpeechbubbleAction"));
+  }
+  cancelActionGoal("RobotSpeechbubbleAction");
+  return result;
 }
 
 //------------------------------------------------------------------------------
@@ -566,5 +623,4 @@ document.getElementById("run").onclick = () => {
 // Scratch
 (async () => {
   console.log("started");
-  conNeckSimple();
 })();
