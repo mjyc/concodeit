254bdf40db034f3d3e28f62be6f9540c5db13968
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-09-02 10:42:55 -0700

    Add Kelvin's neck-exercise

--- condition1-waitforblocks/src/index.js
+++ condition1-waitforblocks/src/index.js
@@ -9,6 +9,7 @@ import {
   makeCancelGoal,
   createStreamEventListener
 } from "cycle-robot-drivers-async";
+import { extractFaceFeatures } from "tabletrobotface-userstudy";
 
 //------------------------------------------------------------------------------
 // Helper Function Definitions
@@ -85,6 +86,53 @@ function waitUntilFaceEvent(id, predicate) {
   })(predicate);
 }
 
+const FaceDirectionChanged = {
+  NOFACE: "noFace",
+  CENTER: "center",
+  LEFT: "left",
+  RIGHT: "right"
+};
+
+const ANGLE = 10; // magnitude of head turn to determine direction
+/* Detects which direction user is facing
+   returns FaceDirectionChanged enum based on user direction
+   inputs:
+     id: process id for this */
+function checkFaceDirection(id) {
+  waitHandles[id] = {
+    listener: null,
+    stream: sources.PoseDetection.events("poses"),
+    stop: () => {
+      waitHandles[id].stream.removeListener(waitHandles[id].listener);
+    }
+  };
+  return promisify(cb => {
+    waitHandles[id].listener = createStreamEventListener(
+      poses => {
+        const faceFeatures = extractFaceFeatures(poses);
+        if (faceFeatures.isVisible) {
+          if (faceFeatures.noseAngle < -1 * ANGLE) {
+            return cb(null, FaceDirectionChanged.RIGHT);
+          } else if (faceFeatures.noseAngle > ANGLE) {
+            return cb(null, FaceDirectionChanged.LEFT);
+          }
+
+          //If nose angle is between -10 and 10, then return center
+          return cb(null, FaceDirectionChanged.CENTER);
+        } else {
+          // If face is not detected, return noFace
+          return cb(null, FaceDirectionChanged.NOFACE);
+        }
+      },
+      (err, val) => {
+        waitHandles[id].stream.removeListener(waitHandles[id].listener);
+        cb(err, val);
+      }
+    );
+    waitHandles[id].stream.addListener(waitHandles[id].listener);
+  })();
+}
+
 function stopWaitUntilFaceEvent(id) {
   waitHandles[id].stop();
 }
@@ -104,7 +152,7 @@ function waitUntilVAD(id) {
         cb(null, val);
       }
     };
-    waitHandles[id].stream.drop(1).addListener(waitHandles[id].listener);
+    waitHandles[id].stream.addListener(waitHandles[id].listener);
   })();
 }
 
@@ -514,8 +562,8 @@ const sources = initialize({
   }
 });
 
-sources.PoseDetection.events("poses").addListener({ next: () => {} });
-sources.VAD.addListener({ next: () => {} });
+sources.PoseDetection.events("poses").addListener({ next: _ => {} });
+sources.VAD.addListener({ next: _ => {} });
 
 document.getElementById("run").onclick = () => {
   var code = `(async () => {${Blockly.JavaScript.workspaceToCode(editor)}})();`;
@@ -523,7 +571,102 @@ document.getElementById("run").onclick = () => {
 };
 
 //------------------------------------------------------------------------------
+
+async function neckExercise() {
+  var instructions, result, i, right, wrong;
+
+  right = [FaceDirectionChanged.RIGHT, FaceDirectionChanged.LEFT];
+  wrong = [FaceDirectionChanged.LEFT, FaceDirectionChanged.RIGHT];
+  instructions = [
+    "Now slowly rotate to your right",
+    "Now slowly rotate to your left"
+  ];
+
+  // beg start_program
+  cancelActionGoals();
+  // end start_program
+  await Promise.race([
+    promisify2(async cb => {
+      result = await sendActionGoal(
+        "RobotSpeechbubbleAction",
+        String("Let's start from looking forward")
+      );
+      cb(null, null);
+    })(),
+    promisify2(async cb => {
+      cb(null, null);
+    })()
+  ]);
+
+  await sleep(1);
+  var direction = await checkFaceDirection(100);
+  while (direction != FaceDirectionChanged.CENTER) {
+    direction = await checkFaceDirection(100);
+  }
+
+  for (var i_index in instructions) {
+    i = instructions[i_index];
+    result = await sendActionGoal("RobotSpeechbubbleAction", String(i));
+    var correct = false;
+    var dir = await checkFaceDirection(1000);
+    await Promise.race([
+      promisify2(async cb => {
+        // Branch that checks for right direction
+        while (dir != right[i_index]) {
+          dir = await checkFaceDirection(1000);
+        }
+        correct = true;
+        cb(null, null);
+      })(),
+      promisify2(async cb => {
+        // Branch that checks and prints wrong direction
+        var dir2 = await checkFaceDirection(111);
+        while (!correct) {
+          // If face is not facing right direction, print and say descriptive comment
+          if (dir2 == wrong[i_index]) {
+            printFalseDir(i);
+          }
+          await sleep(1);
+          dir2 = await checkFaceDirection(111);
+        }
+        cb(null, null);
+      })()
+    ]);
+  }
+  result = await sendActionGoal(
+    "RobotSpeechbubbleAction",
+    String("You're all done!")
+  );
+}
+
+/*
+  Displays and says "try again!" and redisplays instruction i on the screen.
+
+*/
+async function printFalseDir(i) {
+  var result;
+  await Promise.all([
+    promisify2(async cb => {
+      result = await sendActionGoal(
+        "SpeechSynthesisAction",
+        String("Try again!")
+      );
+      cb(null, null);
+    })(),
+    promisify2(async cb => {
+      result = await sendActionGoal(
+        "RobotSpeechbubbleAction",
+        String("Try again!")
+      );
+      cb(null, null);
+    })()
+  ]);
+  await sleep(1);
+  result = await sendActionGoal("RobotSpeechbubbleAction", String(i));
+}
+
 // Scratch
 (async () => {
   console.log("started");
+  await neckExercise();
 })();
