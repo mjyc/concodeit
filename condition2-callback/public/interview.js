2b43ac161e53aace58ab4137da68afc7b22f8cea
Drick <strato95@cs.washington.edu>
2019-10-19 19:30:21 -0700

    three apps

---
+++ condition2-callback/public/interview.js
@@ -0,0 +1,21 @@
+// beg start_program
+cancelActionGoals();
+// end start_program
+setMessage(String('What does a typical day look like for you'));
+waitUntil(String("IsSpeakingTrue"), () => {
+  startFollowingFace();
+  waitUntil(String("FaceDirectionCenter"), () => {
+    waitUntil(String("IsSpeakingFalse"), () => {
+      stopFollowingFace();
+      setMessage(String('What sort of vacations do you like to take'));
+      waitUntil(String("IsSpeakingTrue"), () => {
+        startFollowingFace();
+        waitUntil(String("FaceDirectionCenter"), () => {
+          waitUntil(String("IsSpeakingFalse"), () => {
+            setMessage(String('We are done thank you'));
+          });
+        });
+      });
+    });
+  });
+});
