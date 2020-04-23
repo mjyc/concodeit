d3791baef4e83510cacba1534ab3dff57b1cf70b
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-04-23 14:38:09 -0700

    WIP: playing with viz

--- eval/systematic_evaluation/measure.js
+++ eval/systematic_evaluation/measure.js
@@ -14,7 +14,7 @@ const countNumVariables = (progXMLStr) =>
   (progXMLStr.match(/<variable type=""/g) || []).length;
 
 // Builds dictionary of <block type, # of block type> pairs
-const countBlockByType = (progXMLStr) => {
+const countBlockByType = (progXMLStr, { knownBlockTypes } = {}) => {
   const blocks = progXMLStr.match(/<block type="\w+"/g) || [];
   const counts = {
     variables: countNumVariables(progXMLStr),
@@ -28,6 +28,65 @@ const countBlockByType = (progXMLStr) => {
     }
   }
 
+  const countsOut = Object.assign(
+    {
+      numBlockTypes: Object.keys(counts).length - 1,
+      functions:
+        (counts["when"] || 0) + (counts["procedures_defnoreturn"] || 0),
+    },
+    !knownBlockTypes
+      ? counts
+      : knownBlockTypes.reduce((prev, blockType) => {
+          prev[blockType] = counts[blockType] || 0;
+          return prev;
+        }, {})
+  );
+  return countsOut;
+};
+
+// Returns the maximum depth of a nested scope in the given program xml string
+const getMaxDepth = (progXMLStr) => {
+  // get maximum depth of a nested scope in program
+  let result;
+  const startReg = /<statement name/g,
+    startIndices = [];
+  while ((result = startReg.exec(progXMLStr))) {
+    startIndices.push(result.index);
+  }
+  const endReg = /<\/statement>/g,
+    endIndices = [];
+  while ((result = endReg.exec(progXMLStr))) {
+    endIndices.push(result.index);
+  }
+
+  const stack = [];
+  let s_i = 0;
+  let e_i = 0;
+  let maxDepth = -1;
+  while (e_i < endIndices.length) {
+    let currEnd = endIndices[e_i];
+    while (s_i < startIndices.length && startIndices[s_i] < currEnd) {
+      stack.push(startIndices[s_i++]);
+    }
+    if (s_i < startIndices.length || e_i < endIndices.length) {
+      let start = stack.pop();
+      let subInput = progXMLStr.slice(start, currEnd);
+      let depth = (subInput.match(/<next>/g) || []).length + 1;
+      maxDepth = Math.max(maxDepth, depth);
+      progXMLStr =
+        progXMLStr.substring(0, start) +
+        subInput.replace("<next>", "<abcd>") +
+        progXMLStr.substring(currEnd);
+      e_i++;
+    }
+  }
+  return maxDepth;
+};
+
+const computeMeasures = (progXMLStr) => {
+  const numBlocks = countNumBlocks(progXMLStr);
+  const maxDepth = getMaxDepth(progXMLStr);
+  const numVariables = countNumVariables(progXMLStr);
   const knownBlockTypes = [
     "start_program",
     "say",
@@ -55,34 +114,8 @@ const countBlockByType = (progXMLStr) => {
     "wait_for_event",
     "wait_for_one",
   ];
-  const countsOut = knownBlockTypes.reduce(
-    (prev, blockType) => {
-      prev[blockType] = counts[blockType] || 0;
-      return prev;
-    },
-    {
-      numBlockTypes: Object.keys(counts).length - 1,
-      functions:
-        (counts["when"] || 0) + (counts["procedures_defnoreturn"] || 0),
-    }
-  );
-  return countsOut;
-};
-
-const parseXml = require("@rgrove/parse-xml");
-
-// Returns the maximum depth of a nested scope in the input program xml string
-const getMaxDepth = (progXMLStr) => {
-  const progJSON = parseXml(progXMLStr).toJSON();
-  console.log(JSON.stringify(progJSON, null, 2));
-  return 1;
-};
-
-const computeMeasures = (progXMLStr) => {
-  const numBlocks = countNumBlocks(progXMLStr);
-  const numBlocksByType = countBlockByType(progXMLStr);
-  const maxDepth = getMaxDepth(progXMLStr);
-  return Object.assign({ numBlocks, maxDepth }, numBlocksByType);
+  const numBlocksByType = countBlockByType(progXMLStr, { knownBlockTypes });
+  return Object.assign({ numBlocks, maxDepth, numVariables }, numBlocksByType);
 };
 
 // main
