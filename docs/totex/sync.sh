a929ed60e005c7fe36b09932c718808fc5b3181e
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-04-22 17:19:40 -0700

    re-organize

--- docs/totex/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-read -p "This may overwrite existing files in your paper directory. Are you sure? [Y/n] " -n 1
-echo
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  cp -v tex/*.tex paper/
-fi
