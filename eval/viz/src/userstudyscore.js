8d2ea0f48bb9b321ea9cb93b35e421d502b1f63b
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-04-28 15:23:38 -0700

    WIP

--- eval/viz/src/userstudyscore.js
+++ eval/viz/src/userstudyscore.js
@@ -26,115 +26,19 @@ const main = (data) => {
     data: {
       values: data,
     },
-    transform: [
-      {
-        calculate:
-          "(substring(datum.filename, 0, 1) == 'a') ? 'async' : (substring(datum.filename, 0, 1) == 'c' ) ? 'callback' : 'waitfor'",
-        as: "api",
+    mark: {
+      type: "point",
+    },
+    encoding: {
+      x: {
+        field: "total",
+        type: "quantitative",
       },
-    ],
-    hconcat: [
-      {
-        vconcat: [
-          {
-            xField: "numTotalBlocks",
-            xScale: { domain: [0, 120] },
-            xTitle: "Average Number of Blocks",
-          },
-          {
-            xField: "numFunctions",
-            xScale: { domain: [-2, 12] },
-            xTitle: "Average Number of Functions",
-          },
-          {
-            xField: "numVariables",
-            xScale: { domain: [-2, 12] },
-            xTitle: "Average Number of Variables",
-          },
-        ].map((vspec) => ({
-          layer: [
-            {
-              mark: { type: "point", filled: true, color: "black" },
-              encoding: {
-                x: {
-                  field: vspec.xField,
-                  type: "quantitative",
-                  aggregate: "mean",
-                  scale: vspec.xScale,
-                  title: vspec.xTitle,
-                },
-                y: { field: "api", type: "ordinal", title: null },
-              },
-            },
-            {
-              mark: {
-                type: "errorbar",
-                extent: "stdev",
-              },
-              encoding: {
-                x: {
-                  field: vspec.xField,
-                  type: "quantitative",
-                  aggregate: "mean",
-                  title: vspec.xTitle,
-                },
-                y: { field: "api", type: "ordinal" },
-              },
-            },
-          ],
-        })),
+      y: {
+        field: "total",
+        type: "quantitative",
       },
-      {
-        vconcat: [
-          {
-            xField: "numBranches",
-            xScale: { domain: [-2, 12] },
-            xTitle: "Average Number of Branches",
-          },
-          {
-            xField: "numLoops",
-            xScale: { domain: [-2, 12] },
-            xTitle: "Average Number of Loops",
-          },
-          {
-            xField: "numConds",
-            xScale: { domain: [-2, 12] },
-            xTitle: "Average Number of Conditions",
-          },
-        ].map((vspec) => ({
-          layer: [
-            {
-              mark: { type: "point", filled: true, color: "black" },
-              encoding: {
-                x: {
-                  field: vspec.xField,
-                  type: "quantitative",
-                  aggregate: "mean",
-                  scale: vspec.xScale,
-                  title: vspec.xTitle,
-                },
-                y: { field: "api", type: "ordinal", title: null },
-              },
-            },
-            {
-              mark: {
-                type: "errorbar",
-                extent: "stdev",
-              },
-              encoding: {
-                x: {
-                  field: vspec.xField,
-                  type: "quantitative",
-                  aggregate: "mean",
-                  title: vspec.xTitle,
-                },
-                y: { field: "api", type: "ordinal" },
-              },
-            },
-          ],
-        })),
-      },
-    ],
+    },
   };
   vegaEmbed("#vis", vlSpec);
 };
